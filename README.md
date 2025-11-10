5be0db59f1545121cb587ae5d46c8c8645afc2ce
Alejandro <aleperaltabazas@gmail.com>
2025-11-10 11:53:02 -0300

    adde installer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # conf
+
+```bash
+sudo apt install -y git && \
+    mkdir -p /tmp
+    git clone -C /tmp https://github.com/aleperaltabazas/conf.git && \
+    chmod +x /tmp/conf/install.sh && \
+    /tmp/conf/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp git/.gitconfig ~/.gitconfig
+
+sudo apt install maven
+
+sudo apt install -y redshift redshift-gtk && \
+    cp redshift/redshift.conf ~/.local/share/cinnamon/applets/redshift.conf && \
+    cp redshift/redshift.conf ~/.config/redshift.conf
